1719d05b3d806e5d36940a5fe6ad969744a063b3
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2022-11-03 16:33:00 -0400

    Basic working MPI version

--- bc_mpi.cpp
+++ bc_mpi.cpp
@@ -10,7 +10,6 @@
 #include <unordered_map>
 #include <vector>
 #include <sys/time.h>
-#include <omp.h>
 #include <unistd.h>
 #include <mpi.h>
 
@@ -92,97 +91,83 @@ vector<double> determineBC(AdjList const &graph, int const N, int const data_sta
     int j, v, w, src;
     double pairwise, min_bc = 1, max_bc = 0;
     list<int>::iterator it;
-    #pragma omp parallel shared (bc, bc_local, graph, N, min_bc, max_bc) private (j, v, w, src, pairwise, it)
-    {
-        //printf("Rank %d / %d - Thread %d of %d in BC loop\n", world_rank, world_size, omp_get_thread_num(), omp_get_max_threads());
-        #pragma omp for
-        for (src=data_start; src<=data_end; src++) {
-            //printf("Rank %d / %d - Thread %d of %d - src = %d\n", world_rank, world_size, omp_get_thread_num( ), omp_get_max_threads( ), src);
 
-            // Setup variables for loop
-            stack<int> visited;
-            vector<list<int>> pred(N); // Array of predecessor lists
-            vector<int> sigma(N, 0);
-            vector<int> dist(N, -1);
-            queue<int> frontier;
-            vector<double> delta(N,0);
+    for (src=data_start; src<=data_end; src++) {
+        //printf("Rank %d / %d - Thread %d of %d - src = %d\n", world_rank, world_size, omp_get_thread_num( ), omp_get_max_threads( ), src);
 
-            sigma[src] = 1;
-            dist[src] = 0;
-            frontier.push(src);
+        // Setup variables for loop
+        stack<int> visited;
+        vector<list<int>> pred(N); // Array of predecessor lists
+        vector<int> sigma(N, 0);
+        vector<int> dist(N, -1);
+        queue<int> frontier;
+        vector<double> delta(N,0);
 
-            while (!frontier.empty()) {
-                v = frontier.front();
-                frontier.pop();
-                visited.push(v);
-                //cout << "v = " << v << endl;
+        sigma[src] = 1;
+        dist[src] = 0;
+        frontier.push(src);
 
-                // Get all neighbors of v
-                for (j=0; j<graph.at(v).size(); j++) {
-                    w = graph.at(v)[j];
-                    // has w been found before?
-                    if (dist[w] < 0) {
-                        frontier.push(w);
-                        dist[w] = dist[v] + 1;
-                    }
-                    // shortest path to w via v
-                    if (dist[w] == (dist[v] + 1)) {
-                        sigma[w] += sigma[v];
-                        pred[w].push_back(v);
-                    }
+        while (!frontier.empty()) {
+            v = frontier.front();
+            frontier.pop();
+            visited.push(v);
+            //cout << "v = " << v << endl;
+
+            // Get all neighbors of v
+            for (j=0; j<graph.at(v).size(); j++) {
+                w = graph.at(v)[j];
+                // has w been found before?
+                if (dist[w] < 0) {
+                    frontier.push(w);
+                    dist[w] = dist[v] + 1;
                 }
-            }
-
-            // Stack contains vertices in order of non-increasing distance form s
-            while (!visited.empty()) {
-                w = visited.top();
-                visited.pop();
-                for (it = pred[w].begin(); it != pred[w].end(); it++) {
-                    v = *it;
-                    pairwise = ((double) sigma[v] / (double) sigma[w]) * (1.0 + delta[w]);
-                    delta[v] += pairwise;
+                // shortest path to w via v
+                if (dist[w] == (dist[v] + 1)) {
+                    sigma[w] += sigma[v];
+                    pred[w].push_back(v);
                 }
-
-                if (w != src) {
-                    #pragma omp atomic
-                    bc_local[w] += delta[w];
-                }
-                
             }
         }
 
-        //printf("Rank %d / %d - Thread %d of %d Finished For\n", world_rank, world_size, omp_get_thread_num(), omp_get_max_threads());
-        // REDUCE BC ARRAYS HERE BEFORE DOING THE FINAL OPERATIONS ON MAIN
-        #pragma omp single
-        {
-            //printf("Rank %d / %d - Thread %d of %d Reducing\n", world_rank, world_size, omp_get_thread_num(), omp_get_max_threads());
-            // Only ran by 1 thread, implicit barrier at end
-            MPI_Reduce(bc_local.data(), bc.data(), N, MPI_DOUBLE, MPI_SUM, MAIN_PE, MPI_COMM_WORLD);
-            //printf("Rank %d / %d: Reduction complete\n", world_rank, world_size);
-        }
-
-        // Only the main PE will normalize the data
-        if (world_rank == MAIN_PE) {
-            //printf("Rank %d / %d: Rescale Begun\n", world_rank, world_size);
-
-            // Rescale the BC by dividing by (N-1)(N-2)/2
-            double local_min = 1, local_max = 0;
-            #pragma omp for reduction(min:min_bc) reduction(max:max_bc)
-            for (int i = 0; i < N; i++) {
-                bc[i] /= ((N-1)*(N-2)/2);
-                if (bc[i] < min_bc) min_bc = bc[i];
-                if (bc[i] > max_bc) max_bc = bc[i];
+        // Stack contains vertices in order of non-increasing distance form s
+        while (!visited.empty()) {
+            w = visited.top();
+            visited.pop();
+            for (it = pred[w].begin(); it != pred[w].end(); it++) {
+                v = *it;
+                pairwise = ((double) sigma[v] / (double) sigma[w]) * (1.0 + delta[w]);
+                delta[v] += pairwise;
             }
 
-            //printf("Rank %d / %d: Normalization Begun\n", world_rank, world_size);
-            // Normalize BC between 0 and 1 (Check for divide by zero)
-            if (max_bc != min_bc) {
-                #pragma omp for
-                for (auto &g : bc)
-                    g = (g - min_bc) / (max_bc - min_bc);
+            if (w != src) {
+                bc_local[w] += delta[w];
             }
+            
         }
     }
+
+    // implicit barrier at end
+    MPI_Reduce(bc_local.data(), bc.data(), N, MPI_DOUBLE, MPI_SUM, MAIN_PE, MPI_COMM_WORLD);
+
+    // Only the main PE will normalize the data
+    if (world_rank == MAIN_PE) {
+        //printf("Rank %d / %d: Rescale Begun\n", world_rank, world_size);
+
+        // Rescale the BC by dividing by (N-1)(N-2)/2
+        for (int i = 0; i < N; i++) {
+            bc[i] /= ((N-1)*(N-2)/2);
+            if (bc[i] < min_bc) min_bc = bc[i];
+            if (bc[i] > max_bc) max_bc = bc[i];
+        }
+
+        //printf("Rank %d / %d: Normalization Begun\n", world_rank, world_size);
+        // Normalize BC between 0 and 1 (Check for divide by zero)
+        if (max_bc != min_bc) {
+            for (auto &g : bc)
+                g = (g - min_bc) / (max_bc - min_bc);
+        }
+    }
+
     if (world_rank == MAIN_PE)
         printf("Min = %.5g, Max = %.5g, ", min_bc, max_bc);
 
@@ -206,11 +191,10 @@ int main(int argc, char* argv[]) {
     // ==================== //
     //     Cmd line args    //
     // ==================== //
-	// #Cores, #Trials, *graphname*
-	// ./bc_hybrid 8 10 small_example
-	int cores = atoi(argv[1]);
-    int trials = atoi(argv[2]);
-    string graphName(argv[3]);
+	// #Trials, *graphname*
+	// ./bc_hybrid 5 small_example
+    int trials = atoi(argv[1]);
+    string graphName(argv[2]);
     string ifname = "./graphs/" + graphName + ".txt";
 
     // ==================== //
@@ -223,9 +207,9 @@ int main(int argc, char* argv[]) {
     NodeTranslator nodeList;
     double load_start, load_end;
     int N = 0, E = 0;
-    load_start = omp_get_wtime();
+    load_start = MPI_Wtime();
     AdjList graph = loadGraph(ifname, nodeList, N, E);
-    load_end = omp_get_wtime();
+    load_end = MPI_Wtime();
     if (world_rank == MAIN_PE) {
         printf("N = %d, E=%d\n", N, E);
         printf("Graph Load time: %f\n", load_end - load_start);
@@ -248,10 +232,9 @@ int main(int argc, char* argv[]) {
     // ==================== //
     //      BC Testing      //
     // ==================== //
-    omp_set_num_threads(cores);
     if (world_rank == MAIN_PE) {
         printf("\n----- BC Results -----\n");
-        printf("Nodes = %d, Threads = %d\n", world_size, omp_get_max_threads());
+        printf("Nodes = %d\n", world_size);
     }
     double start, end;
     vector<double> bc;
@@ -259,11 +242,11 @@ int main(int argc, char* argv[]) {
         MPI_Barrier(MPI_COMM_WORLD); // Ensure all nodes start at the same time
         if (world_rank == MAIN_PE) {
             printf("Trial %d, ", i);
-            start = omp_get_wtime();
+            start = MPI_Wtime();
         }
         bc = determineBC(graph, N, data_start, data_end);
         if (world_rank == MAIN_PE) {
-            end = omp_get_wtime();
+            end = MPI_Wtime();
             printf("Time: %f\n", end - start);
         }
     }
@@ -275,8 +258,8 @@ int main(int argc, char* argv[]) {
     #if SAVE_RESULTS
         if (world_rank == MAIN_PE) {
             double save_start, save_end;
-            save_start = omp_get_wtime();
-            string ofname = "./outputs/" + graphName + "-mpi-" + to_string(world_size) + "-" + to_string(omp_get_max_threads()) + ".txt";
+            save_start = MPI_Wtime();
+            string ofname = "./outputs/" + graphName + "-mpi-" + to_string(world_size) + ".txt";
             ofstream outfile;
             outfile.open(ofname, ios::out | ios::trunc);
 
@@ -286,7 +269,7 @@ int main(int argc, char* argv[]) {
 
             printf("\n----- Saving Data -----\n");
             printf("BC Data: %s\n", ofname.c_str());
-            outfile << "# of Threads: " << omp_get_max_threads() << endl;
+            outfile << "# of Nodes: " << world_size << endl;
             outfile << "Betweenness runtime: " << (end - start) << endl;
             outfile << "BC results:" << endl;
             outfile << "Node # \t| Betweenness" << endl;
@@ -294,7 +277,7 @@ int main(int argc, char* argv[]) {
                 outfile << nodesOrdered[i] << " \t| " << bc[i] << endl;
             }
             outfile.close();
-            save_end = omp_get_wtime();
+            save_end = MPI_Wtime();
             printf("Save time: %f\n", save_end - save_start);
         }
     #endif
