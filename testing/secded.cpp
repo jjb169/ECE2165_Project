44ddc21ab46d698aeed1dee3ad271591537c7d76
James Bickerstaff <jjb169@pitt.edu>
2022-12-13 01:07:04 -0500

    Wrap up SECDED code, placed in header
    I just slapped the code for the whole secded process within the "secded.h" file bc why not. Added a step by step process in comments for how to go about using the code as well
    
    Also created "secded_h_test.cpp" just to verify that calling functions from the header works

--- testing/secded.cpp
+++ testing/secded.cpp
@@ -6,13 +6,12 @@
 #include <iostream>
 #include <inttypes.h>
 #include <cstdlib>
-#include "secded.h"
 
 #define DEBUG 0
 #define BIT_DEBUG 0
 #define PRINTINFO 0
 
-/* P matrix and syndromes created by https://github.com/jgaeddert/liquid-dsp/blob/master/src/fec/src/fec_secded7264.c*/
+/* P matrix and syndromes created by https://github.com/jgaeddert/liquid-dsp/blob/master/src/fec/src/fec_secded7264.c */
 
 // P matrix [8 x 64] - H matrix if you add on the Identity matrix
 //  11111111 00001111 00001111 00001100 01101000 10001000 10001000 10000000 : 1 0 0 0 0 0 0 0 ; t65=s63^...^s56^s51^...^s48^s43^...^s40^s35^s34^s30^s29^s27^s23^s19^s15^s11^s7
@@ -286,8 +285,287 @@ bool secded_data(unsigned char data[], unsigned char syndrome)
 }
 
 
-//main function to test external methods
+//function to take in a double, add the check byte, and place the results in a passed in char array
+//inputs:
+// 1) a double that contains the data being transmitted
+// 2) an UNSIGNED char array of NINE (9) indices to hold 8 bytes of data and 1 byte of check (64 bits + 8 = 72 bits for (72,64) Hamming code)
+//output:
+// just modifying the input char array directly, no return values
+void addCheckByte(double data, unsigned char codeWord[])
+{	
+	//turn the input data into a byte array
+	unsigned char *dataBytes = (unsigned char *)(&data);	
+	
+	//message to transmit: will be a char array[9] where [0] is check bits and [1-8] are data bits
+	codeWord[0] = 0;
+	//assign data bytes now
+	for(int i = 1; i < 9; i++)
+	{
+		codeWord[i] = *(dataBytes+i-1);
+	}
+	
+	//need to determine the byte to add on through parity checks
+	appendCheck(codeWord);
+	
+	//parity byte added to char array, ready to transmit
+}
+
+
+//function to modify bits - currently only 1 or 2
+//inputs:
+// 1) codeWord in the form of unsigned char array -> 9 bytes
+// 2) chance of a SINGLE bit error occurring, just using an int for "X mod chance" calculation
+// 3) chance of a DOUBLE bit error occurring, just using an int for "X mod chance" calculation
+//output:
+// returning the number of modified bits
+// modifying the codeWord array directly to induce bit errors
+int modifyBits(unsigned char codeWord[], int singleErrorChance, int doubleErrorChance)
+{
+	//var to return how many bits were modified
+	int modifiedCount = 0; 
+	
+	//alter data in some way
+	//check if random value generated mod error chance is 0, if so, add a single bit error
+	if(std::rand() % singleErrorChance == 0)
+	{
+		//increment bit error count
+		modifiedCount++;
+		//first select which byte and bit to modify
+		int byte = std::rand() % 9; //byte to alter
+		int bit = std::rand() % 8; //bit withihn that byte
+		//if 0, change to 1, else it is a 1, change to 0
+		if(codeWord[byte] & (0x01 << bit) == 0)
+		{
+			codeWord[byte] = codeWord[byte] | (0x01 << bit);
+		}
+		else
+		{
+			codeWord[byte] = codeWord[byte] ^ (0x01 << bit);
+		}
+		
+		//determine if doing double error as well, doing mod doubleErrorChance 
+		if(std::rand() % doubleErrorChance == 0)
+		{
+			//increment bit error count
+			modifiedCount++;
+			
+			//save old byte/bit values so we bit flip something else instead of flipping it back to normal
+			int byteTemp = byte, bitTemp = bit;
+			
+			//once again generate a byte and bit to mess with
+			while(byteTemp == byte)
+				byte = std::rand() % 9; //byte to alter
+			while(bitTemp == bit)
+				bit = std::rand() % 8; //bit withihn that byte
+			//if 0, change to 1, else it is a 1, change to 0
+			if(codeWord[byte] & (0x01 << bit) == 0)
+			{
+				codeWord[byte] = codeWord[byte] | (0x01 << bit);
+			}
+			else
+			{
+				codeWord[byte] = codeWord[byte] ^ (0x01 << bit);
+			}
+		}
+	}
+	
+	//return # of modified bits
+	return modifiedCount;	
+}
+
+
+//modify to check - and possibly correct the received data after a transmission
+//input:
+// 1) receivedData is the data from a transmission that may or may not be modified, in form of unsigned char array
+//output:
+// bool signifying whether the data is correct or could not be fixed
+// also directly fix the data array of received data
+bool checkReceivedData(unsigned char receivedData[])
+{	
+	//bool for if data has been fixed (or not needed fixed) or needs retransmitted (DED)
+	bool correctData = false;
+	//calculate syndrome based on received data
+	unsigned char syndrome = calculateSyndrome(receivedData);
+	
+	//do all the following if syndrome != 0, meaning no error
+	if(syndrome != 0x00)
+	{
+		//correct bit if possible
+		correctData = secded_data(receivedData, syndrome);
+		
+		//if it could not be corrected,double error has been detected
+		if(correctData)
+		{
+			//DATA SUCCESSFULLY FIXED :D
+			//dont really need to do anything else, bool already set to true for return
+			correctData = true;
+		}
+		else
+		{
+			//DED DETECTED, NEED TO RETRANSMIT DATA
+			correctData = false;
+		}		
+	}
+	else
+	{
+		//NO ERROR DETECTED, DATA NEEDS NO MODIFICATION
+		correctData = true;
+	}
+	
+	//return bool signifying if data is good or not
+	return correctData;
+}
+
+//simple method to retranslate data from unsigned char array back to a double
+//input:
+// 1) unsigned char array containing 9 bytes of 8 data + 1 check byte
+//output:
+// returns the data received and fixed as a double, as needed by program
+double charToDouble(unsigned char receivedData[])
+{
+	//want just bytes 1 through 8, as byte 0 is a check
+	unsigned char originalData[8];
+	for(int i = 1, j = 0; i < 9; i++, j++)
+	{
+		//copy byte by byte
+		originalData[j] = receivedData[i];
+	}
+	//turn the original 8 bytes back into a double
+	double fixedData = *(double*)originalData;
+	
+	//return this original data
+	return fixedData;
+}
+
+
+//modified main function to make use of methods for each step, as will be done in actual program
 int main(int argc, char** argv)
+{
+	//grab iteration count from cmd
+	if(argc < 4 || argc > 4)
+	{
+		printf("Please have 3 arguments: # iterations for fault injection, mod for single error chance, mod for double error chance\n");
+		exit(0);
+	}
+	//grab iteration count
+	int totalIterations = std::atoi(argv[1]);
+	int singleErrorChance = std::atoi(argv[2]);
+	int doubleErrorChance = std::atoi(argv[3]);
+	
+	//seed rng
+	std::srand(time(0));
+	
+	//PHASE 1: TAKE INPUT AND ADD CHECK BITS
+	
+	//take a double as input from keyboard for testing
+	//actual variable
+	double test;
+	unsigned char *bytes;
+	printf("Please input a number to test: ");
+	std::cin >> test;
+	bytes = (unsigned char *)(&test);	
+	//message to transmit: will be a char array[9] where [0] is check bits and [1-8] are data bits
+	unsigned char transmit[9];
+	//send in double and char array to add check bits
+	addCheckByte(test, transmit);
+	
+	//SETUP FOR LOOPING PHASES 2 AND 3 (FAULT INJECTION AND CORRECTION)
+	
+	//counting vars
+	int se_corrections = 0, ded_count = 0, no_error_count = 0;
+	int se_faults = 0, ded_faults = 0;
+	int iters = 0;
+	
+	//do this X times for testing
+	for(iters = 0; iters < totalIterations; iters++)
+	{
+		//copy data for this iteration
+		unsigned char copy[9];
+		for(int j = 0; j < 9; j++)
+		{
+			copy[j] = transmit[j];
+		}
+	
+		//PHASE 2: INJECT A FAULT OR TWO
+		
+		int numBitsModified = modifyBits(copy, singleErrorChance, doubleErrorChance);
+		//update counter variables for output verification
+		if(numBitsModified == 1)
+		{
+			se_faults++;
+		}
+		else if(numBitsModified == 2)
+		{
+			ded_faults++;
+		}
+		
+		//PHASE 3: TAKE IN MODIFIED DATA, CALCULATE SYNDROME AND DECIDE NEXT COURSE OF ACTION
+		
+		bool corrected = checkReceivedData(copy);
+		
+		//check result of correction attempt
+		
+		//4 cases:
+		//corrected == true and numBitsModified == 0, meaning no error detected
+		//corrected == true and numBitsModified == 1, meaning single bit error fixed
+		//corrected == false and numBitsModified == 2, meaning double bit error detected, cannot correct
+		//corrected == false and numBitsModified != 2, meaning we missed a correction
+		if(corrected && numBitsModified == 0)
+		{
+			no_error_count++;
+			//also want to turn data back into a double and make sure it matches original input
+			double correctedData = charToDouble(copy);
+			if(test != correctedData)
+			{
+				printf("NO MODIFICATIONS BUT TRANSLATED DATA DOES NOT MATCH ORIGINAL INPUT!\n");
+			}
+		}
+		else if(corrected && numBitsModified == 1)
+		{
+			se_corrections++;
+			//also want to turn data back into a double and make sure it matches original input
+			double correctedData = charToDouble(copy);
+			if(test != correctedData)
+			{
+				printf("CORRECTED DATA DOES NOT MATCH ORIGINAL INPUT!\n");
+			}
+		}
+		else if (!corrected && numBitsModified == 2)
+		{
+			ded_count++;
+		}
+		else
+		{
+			//missed correcting or detecting an error
+			printf("MISSED AN ERROR!!\n");
+			printf("Original data: ");
+			for(int i = 8; i >= 0; i--)
+			{
+				printf("%02X",transmit[i] & 0xFF);
+			}
+			printf("Modified data: ");
+			for(int i = 8; i >= 0; i--)
+			{
+				printf("%02X",copy[i] & 0xFF);
+			}
+			printf("\n");
+		}
+	}
+	
+	//done testing, output results
+	//print results
+	printf("Out of %d iterations:\n", iters);
+	printf("Single Errors injected: %d\n", se_faults);
+	printf("Single Error corrections: %d\n", se_corrections);
+	printf("Double Error detections: %d\n", ded_count);
+	printf("Double Errors injected: %d\n", ded_faults);
+	printf("No Errors found: %d\n", no_error_count);
+}
+
+
+/*
+//main function to test external methods - DONT USE/ONLY USE FOR VERBOSE DEBUGGING PURPOSES
+int mainBackup(int argc, char** argv)
 {
 	
 	//grab iteration count from cmd
@@ -358,6 +636,8 @@ int main(int argc, char** argv)
 		printf("%02X",transmit[i] & 0xFF);
 	}
 	printf("\n\n");
+	
+	//END PHASE 1
 	
 	//counting vars
 	int se_corrections = 0, ded_count = 0, no_error_count = 0;
@@ -594,6 +874,12 @@ int main(int argc, char** argv)
 			}
 		}
 	}
+	
+	//trying to turn the bytes array back into a double
+	//double testTwo = *(double*)bytes;
+	//printf("byte array back into double = %.3lf\n", testTwo);
+	//printf("test + testTwo = %.3lf\n", testTwo+test);
+	
 	//print results
 	printf("Note: single errors turn into double errors, so \nsingle error injection count = single errors injected - double errors injected\n\n");
 	printf("Out of %d iterations:\n", iters);
@@ -605,4 +891,4 @@ int main(int argc, char** argv)
 	
 	return 0;
 }
-
+*/
---
+++ testing/secded_h_test.cpp
@@ -0,0 +1,130 @@
+//james bickerstaff
+//testing basic double data type functions for SEC-DED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <inttypes.h>
+#include <cstdlib>
+#include "secded.h"
+
+int main(int argc, char **argv)
+{
+	//grab iteration count from cmd
+	if(argc < 4 || argc > 4)
+	{
+		printf("Please have 3 arguments: # iterations for fault injection, mod for single error chance, mod for double error chance\n");
+		exit(0);
+	}
+	//grab iteration count
+	int totalIterations = std::atoi(argv[1]);
+	int singleErrorChance = std::atoi(argv[2]);
+	int doubleErrorChance = std::atoi(argv[3]);
+	
+	//seed rng
+	std::srand(time(0));
+	
+	//PHASE 1: TAKE INPUT AND ADD CHECK BITS
+	
+	//take a double as input from keyboard for testing
+	//actual variable
+	double test;
+	printf("Please input a number to test: ");
+	std::cin >> test;
+	//message to transmit: will be a char array[9] where [0] is check bits and [1-8] are data bits
+	unsigned char transmit[9];
+	//send in double and char array to add check bits
+	addCheckByte(test, transmit);
+	
+	//SETUP FOR LOOPING PHASES 2 AND 3 (FAULT INJECTION AND CORRECTION)
+	
+	//counting vars
+	int se_corrections = 0, ded_count = 0, no_error_count = 0;
+	int se_faults = 0, ded_faults = 0;
+	int iters = 0;
+	
+	//do this X times for testing
+	for(iters = 0; iters < totalIterations; iters++)
+	{
+		//copy data for this iteration
+		unsigned char copy[9];
+		for(int j = 0; j < 9; j++)
+		{
+			copy[j] = transmit[j];
+		}
+	
+		//PHASE 2: INJECT A FAULT OR TWO
+		
+		int numBitsModified = modifyBits(copy, singleErrorChance, doubleErrorChance);
+		//update counter variables for output verification
+		if(numBitsModified == 1)
+		{
+			se_faults++;
+		}
+		else if(numBitsModified == 2)
+		{
+			ded_faults++;
+		}
+		
+		//PHASE 3: TAKE IN MODIFIED DATA, CALCULATE SYNDROME AND DECIDE NEXT COURSE OF ACTION
+		
+		bool corrected = checkReceivedData(copy);
+		
+		//check result of correction attempt
+		
+		//4 cases:
+		//corrected == true and numBitsModified == 0, meaning no error detected
+		//corrected == true and numBitsModified == 1, meaning single bit error fixed
+		//corrected == false and numBitsModified == 2, meaning double bit error detected, cannot correct
+		//corrected == false and numBitsModified != 2, meaning we missed a correction
+		if(corrected && numBitsModified == 0)
+		{
+			no_error_count++;
+			//also want to turn data back into a double and make sure it matches original input
+			double correctedData = charToDouble(copy);
+			if(test != correctedData)
+			{
+				printf("NO MODIFICATIONS BUT TRANSLATED DATA DOES NOT MATCH ORIGINAL INPUT!\n");
+			}
+		}
+		else if(corrected && numBitsModified == 1)
+		{
+			se_corrections++;
+			//also want to turn data back into a double and make sure it matches original input
+			double correctedData = charToDouble(copy);
+			if(test != correctedData)
+			{
+				printf("CORRECTED DATA DOES NOT MATCH ORIGINAL INPUT!\n");
+			}
+		}
+		else if (!corrected && numBitsModified == 2)
+		{
+			ded_count++;
+		}
+		else
+		{
+			//missed correcting or detecting an error
+			printf("MISSED AN ERROR!!\n");
+			printf("Original data: ");
+			for(int i = 8; i >= 0; i--)
+			{
+				printf("%02X",transmit[i] & 0xFF);
+			}
+			printf("Modified data: ");
+			for(int i = 8; i >= 0; i--)
+			{
+				printf("%02X",copy[i] & 0xFF);
+			}
+			printf("\n");
+		}
+	}
+	
+	//done testing, output results
+	//print results
+	printf("Out of %d iterations:\n", iters);
+	printf("Single Errors injected: %d\n", se_faults);
+	printf("Single Error corrections: %d\n", se_corrections);
+	printf("Double Error detections: %d\n", ded_count);
+	printf("Double Errors injected: %d\n", ded_faults);
+	printf("No Errors found: %d\n", no_error_count);
+}
